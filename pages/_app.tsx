fded8835076361b839193f43a3a589f8648c4983
Héricles Emanuel <hericles@getupcloud.com>
2022-10-30 19:40:37 -0300

    new: feat: setup google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PJFZBEEJJ6"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PJFZBEEJJ6');
+        `}
+      </Script>
+
       <Component {...pageProps} />
       <Analytics />
     </>
